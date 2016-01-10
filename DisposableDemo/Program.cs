55a25fad6f573580e076095e8693434bd557a065
Licshee <licshee@yen.ai>
2016-01-10 23:10:18 +0800

    .

---
+++ DisposableDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DisposableDemo
+{
+    using static Console;
+
+    class Program : DisposableBase
+    {
+        static void Main(string[] args)
+        {
+            using (var instance = new Program())
+                instance.Dispose();
+
+            new Program();
+            GC.Collect();
+
+            ReadKey();
+        }
+
+        protected override void Dispose(bool disposing)
+            => WriteLine(disposing);
+    }
+}
